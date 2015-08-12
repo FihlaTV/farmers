6f57fc216e5f9f677254e43a49a0f0fbf480369b
Killer57575 <Killer57575@gmail.com>
2015-08-12 14:23:52 +0300

    helpers/import.js

---
+++ helpers/import.js
@@ -0,0 +1,38 @@
+/**
+ * Created by kille on 12.08.2015.
+ */
+var csv = require('csv');
+var fs = require('fs');
+
+var  parseCsvData = function (csvFile, callback) {
+
+    var attributes; //first row - header of csv data
+
+    fs.readFile(csvFile.path, 'utf8', function (err, stringFileData) {
+        if (err) {
+            callback(err);
+        } else {
+            csv.parse(stringFileData,{delimiter: '|'}, function (err, parsedData) {
+                if (err) {
+                    callback(err);
+                }
+                csv.transform(parsedData,
+                    function (row) {
+                        if (!attributes) {
+                            attributes = row;
+                            return null;
+                        }
+                        return row;
+                    },
+                    function (err, rows) {
+                        var jsonData = _.map(rows, function (row) {
+                            return _.object(attributes, row);
+                        });
+                        callback(null, jsonData, attributes);
+                    })
+            })
+        }
+    });
+};
+
+module.exports = parseCsvData;
