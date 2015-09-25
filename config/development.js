4efc6c1cc0ef6961a139362ed665d48d7ac80be0
darkvas <darkvas90@gmail.com>
2015-09-25 11:04:36 +0300

    added host and port for dev config

--- config/development.js
+++ config/development.js
@@ -1,5 +1,6 @@
+process.env.HOST = 'localhost';
+process.env.PORT = 7792;
+
 process.env.DB_HOST = 'localhost';
-process.env.DB_NAME = "Farmers";
+process.env.DB_NAME = 'Farmers';
 process.env.DB_PORT = 27017;
-
-
