e246986fc7088c1df079f233dc5f8320d29c2c9e
wolfmanzeroua <12345678i>
2015-11-25 09:25:32 +0200

    Customer SendGrid account

--- config/development.js
+++ config/development.js
@@ -6,8 +6,8 @@ process.env.DB_NAME = 'Farmers';
 process.env.DB_PORT = 27017;
 
 process.env.MAIL_SERVICE = 'SendGrid';
-process.env.MAIL_USERNAME = 'istvan.nazarovits';
-process.env.MAIL_PASSWORD = 'sendGridpassw365';
+process.env.MAIL_USERNAME = 'midonabe';
+process.env.MAIL_PASSWORD = 'midonabe80';
 
 process.env.IMAGE_UPLOADER_CONFIG = {
 
